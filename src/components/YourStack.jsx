e61f92c9222368fb3a9fe8e626992bee06d70100
shahed-ab <abshahed094@gmail.com>
2026-09-13 17:55:03 +0600

    Fix remove icon size too small in YourStack

--- src/components/YourStack.jsx
+++ src/components/YourStack.jsx
@@ -52,7 +52,7 @@ export default function YourStack({ stack, onRemove, onRemoveAll }) {
                   aria-label={`Remove ${item.name}`}
                 >
                   <svg
-                    className="w-4 h-4"
+                    className="w-4 h-5"
                     fill="none"
                     stroke="currentColor"
                     viewBox="0 0 24 24"
